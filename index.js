a8dde6fe13d718090aac553c044df720a7ad6229
achyut2000 <medehalachyut@gmail.com>
2021-11-25 08:19:26 +0530

    modify

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ footballStats.liveMatches = () => {
         };
 
         axios.request(options).then(function (response) {
-            console.log(response.data);
+            return (response.data);
         }).catch(function (error) {
             console.error(error);
         });
